ccc2a0284b6d0c667eaa26c51d994f760e258347
Kaarthik Annamalai <kaarthik.alagappan@gmail.com>
2021-06-22 20:26:50 -0400

    adding ability to pass in existing spotify client

--- scheduled_job.py
+++ scheduled_job.py
@@ -4,47 +4,48 @@ from os import path
 import configparser
 
 
-def shufflePlaylist(playlistURI):
-    # getting the default cache path (from spotshuffle)
-    scriptPath = path.dirname(path.realpath(__file__))
-    cachePath = scriptPath+'/spotifytokencache'
+def shufflePlaylist(playlistURI, spotify_client = None):
+    if spotify_client == None:
+        # getting the default cache path (from spotshuffle)
+        scriptPath = path.dirname(path.realpath(__file__))
+        cachePath = scriptPath+'/spotifytokencache'
 
-    scope = 'user-library-read playlist-modify-public playlist-modify-private'
+        scope = 'user-library-read playlist-modify-public playlist-modify-private'
 
-    config = configparser.ConfigParser()
-    config.read(scriptPath+'/config.cfg')
+        config = configparser.ConfigParser()
+        config.read(scriptPath+'/config.cfg')
 
-    # making sure authorization token is present to avoid making user login
-    if not path.isfile(cachePath):
-        print("Cached authorization token not found, please run spotshuffle program first.")
-        exit(1)
+        # making sure authorization token is present to avoid making user login
+        if not path.isfile(cachePath):
+            print("Cached authorization token not found, please run spotshuffle program first.")
+            exit(1)
 
-    # Tries to authorize the user with cached token
-    try:
-        spotify = spotipy.Spotify(
-            auth_manager=spotipy.oauth2.SpotifyOAuth(
-                client_id=config.get('APPDATA', 'CLIENT_ID'),
-                client_secret=config.get('APPDATA', 'CLIENT_SECRET'),
-                redirect_uri=config.get('APPDATA', 'REDIRECT_URI'),
-                scope=scope,
-                cache_path=cachePath
+        # Tries to authorize the user with cached token
+        try:
+            spotify_client = spotipy.Spotify(
+                auth_manager=spotipy.oauth2.SpotifyOAuth(
+                    client_id=config.get('APPDATA', 'CLIENT_ID'),
+                    client_secret=config.get('APPDATA', 'CLIENT_SECRET'),
+                    redirect_uri=config.get('APPDATA', 'REDIRECT_URI'),
+                    scope=scope,
+                    cache_path=cachePath
+                )
             )
-        )
-    except spotipy.oauth2.SpotifyOauthError:
-        print("Unable to obtain Spotify permissions with cached token")
-        exit(1)
+        except spotipy.oauth2.SpotifyOauthError:
+            print("Unable to obtain Spotify permissions with cached token")
+            exit(1)
 
     songIDList = []
 
     # retrieve all tracks in the playlist
-    playlistContents = spotify.playlist_items(playlistURI)
+    playlistContents = spotify_client.playlist_items(playlistURI)
     for song in playlistContents['items']:
         songIDList.append(song['track']['id'])
 
     # shuffle the tracks and save it
     random.shuffle(songIDList)
 
-    spotify.playlist_replace_items(playlistURI, songIDList)
+    spotify_client.playlist_replace_items(playlistURI, songIDList)
 
 
 if __name__ == "__main__":
